c61bd4f4bb91f7e25b172f06d6be763eac69ae50
redaezziani <klausdev2@gmail.com>
2024-10-25 14:34:16 +0100

    Refactor main.go to improve manga retrieval and storage logic

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"database/sql" // Ensure this line is present
+	"database/sql" 
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,8 +10,6 @@ import (
 	"manga-scraper-api/lib/middleware"
 	"net/http"
 	"os"
-	"os/exec"
-	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -69,13 +67,23 @@ func initDB() *sql.DB {
 }
 
 
-func isMangaAlreadyExist(title string) bool {
-    var count int
-    row := db.QueryRow(`SELECT COUNT(*) FROM rate_limits WHERE manga_title = ?`, title)
-    if err := row.Scan(&count); err != nil {
-        return false
-    }
-    return count > 0
+type MangaResponse struct {
+    Exists     bool   `json:"exists"`
+    MangaTitle string `json:"manga_title,omitempty"`
+}
+
+func isMangaAlreadyExist(title, chapter string) (bool, string, error) {
+	var count int
+	var mangaTitle string
+
+	// Query the database
+	row := db.QueryRow(`SELECT COUNT(*), manga_title FROM rate_limits WHERE manga_title = ? AND chapter_number = ?`, title, chapter)
+	err := row.Scan(&count, &mangaTitle) 
+	if err != nil {
+		return false, "", err 
+	}
+
+	return count > 0, mangaTitle, nil 
 }
 
 func getManga(w http.ResponseWriter, r *http.Request) {
@@ -88,30 +96,37 @@ func getManga(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Query().Get("title")
 	chapter := r.URL.Query().Get("chapter")
 	mode := r.URL.Query().Get("mode")
-	clientIP := r.RemoteAddr // Get the client's IP address
+	clientIP := r.RemoteAddr 
 
 	if title == "" {
 		http.Error(w, "Title query parameter is required", http.StatusBadRequest)
 		return
 	}
 
-    isMangaDownloaded := isMangaAlreadyExist(title)
+    isMangaDownloaded, existingTitle,fail := isMangaAlreadyExist(title, chapter)
+    if fail != nil {
+        http.Error(w, "Failed to check if manga is already downloaded", http.StatusInternalServerError)
+        return
+    }
     if isMangaDownloaded {
-		// Let's add +1 to the request count
-		_, err := db.Exec(`UPDATE rate_limits SET request_count = request_count + 1 WHERE manga_title = ?`, title)
+        stm := `UPDATE rate_limits SET request_count = request_count + 1 WHERE manga_title = ?`
+		_, err := db.Exec(stm, title)
 		if err != nil {
 			http.Error(w, "Failed to update request count", http.StatusInternalServerError)
 			return
 		}
 
 		if mode == "download" {
-            new_title := strings.ReplaceAll(title, " ", "_")
-			http.Redirect(w, r, fmt.Sprintf("/d?manga=%s", new_title), http.StatusSeeOther)
+            
+			http.Redirect(w, r, fmt.Sprintf("/d?manga=%s", existingTitle), http.StatusSeeOther)
 			return
 		}
 	} else {
-		_, err := db.Exec(`INSERT INTO rate_limits (client_ip, request_count, chapter_number, manga_title, last_request_timestamp, timestamp) VALUES (?, ?, ?, ?, ?, ?)`,
-			clientIP, 1, chapter, title, time.Now(), time.Now())
+        stm := `INSERT INTO rate_limits (client_ip, request_count, chapter_number, manga_title, last_request_timestamp, timestamp) VALUES (?, ?, 
+              ?, ?, ?, ?)`
+        new_title := strings.ReplaceAll(title, " ", "_")
+		_, err := db.Exec(stm,
+			clientIP, 1, chapter, new_title, time.Now(), time.Now())
 		if err != nil {
 			http.Error(w, "Failed to insert rate limit data", http.StatusInternalServerError)
 			return
@@ -135,64 +150,37 @@ func getManga(w http.ResponseWriter, r *http.Request) {
 	folderName := fmt.Sprintf("images/%s", title)
 	os.MkdirAll(folderName, os.ModePerm)
 
-	// Store images and their paths in the database
 	for _, image := range images {
 		if err := lib.SaveImage(image.Src, folderName, image.DataIndex); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
+        stm := `INSERT INTO manga_images (title, chapter, image_path) VALUES (?, ?, ?)`
         new_chapter, _ := strconv.Atoi(chapter)
-		_, err := db.Exec(`INSERT INTO manga_images (title, chapter, image_path) VALUES (?, ?, ?)`, title, new_chapter, fmt.Sprintf("%s/%d.jpg", folderName, image.DataIndex))
+		_, err := db.Exec(stm, title, new_chapter, fmt.Sprintf("%s/%d.jpg", folderName, image.DataIndex))
 		if err != nil {
 			http.Error(w, "Failed to store image in database", http.StatusInternalServerError)
 			return
 		}
 	}
+    stm := `INSERT INTO rate_limits (client_ip, request_count, chapter_number, manga_title, last_request_timestamp, 
+           timestamp) VALUES (?, ?, ?, ?, ?, ?)`
 
-	// Insert rate limit data into the database
-	_, err = db.Exec(`INSERT INTO rate_limits (client_ip, request_count, chapter_number, manga_title, last_request_timestamp, timestamp) VALUES (?, ?, ?, ?, ?, ?)`,
-		clientIP, 1, chapter, title, time.Now(), time.Now())
+    new_manga_title := strings.ReplaceAll(title, " ", "_")
+	_, err = db.Exec(stm, clientIP, 1, chapter, new_manga_title, time.Now(), time.Now())
 	if err != nil {
 		http.Error(w, "Failed to insert rate limit data", http.StatusInternalServerError)
 		return
 	}
 
-	if err := lib.GeneratePDF(folderName, title); err != nil {
+	if err := lib.GeneratePDF(folderName, new_manga_title); err != nil {
 		http.Error(w, "Failed to generate PDF", http.StatusInternalServerError)
 		return
 	}
 
 	if mode == "download" {
-		// Redirect to the download endpoint
 		http.Redirect(w, r, fmt.Sprintf("/d?manga=%s", title), http.StatusSeeOther)
 		return
-	} else if mode == "open" {
-		newTitle := strings.ReplaceAll(title, " ", "_")
-		pdfFilePath := fmt.Sprintf("manga-pdf/pdf/%s.pdf", newTitle)
-
-		if _, err := os.Stat(pdfFilePath); os.IsNotExist(err) {
-			http.Error(w, "PDF file does not exist", http.StatusNotFound)
-			return
-		}
-
-		// Check the OS
-		var cmd *exec.Cmd
-		if runtime.GOOS == "windows" {
-			cmd = exec.Command("start", pdfFilePath)
-		} else if runtime.GOOS == "linux" {
-			cmd = exec.Command("xdg-open", pdfFilePath)
-		} else if runtime.GOOS == "darwin" {
-			cmd = exec.Command("open", pdfFilePath)
-		} else {
-			http.Error(w, "Unsupported operating system", http.StatusInternalServerError)
-			return
-		}
-
-		if err := cmd.Start(); err != nil {
-			http.Error(w, "Failed to open PDF in GUI", http.StatusInternalServerError)
-			return
-		}
 	}
 
 	response := Response{
@@ -209,17 +197,14 @@ func downloadPDF(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Sanitize the title for file path
 	newTitle := strings.ReplaceAll(title, " ", "_")
 	pdfFilePath := fmt.Sprintf("manga-pdf/pdf/%s.pdf", newTitle)
 
-	// Check if the PDF file exists
 	if _, err := os.Stat(pdfFilePath); os.IsNotExist(err) {
 		http.Error(w, "PDF file does not exist", http.StatusNotFound)
 		return
 	}
 
-	// Set headers for download
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.pdf", newTitle))
 	w.Header().Set("Content-Type", "application/pdf")
 
@@ -235,13 +220,12 @@ func init() {
 }
 
 func main() {
-	rateLimiter := middleware.NewRateLimiter(1, 5*time.Second) // 1 request per 5 seconds rate limit
+	rateLimiter := middleware.NewRateLimiter(1, 5*time.Second) 
 	http.Handle("/manga", rateLimiter.Limit(http.HandlerFunc(getManga)))
-	http.HandleFunc("/d", downloadPDF) // Handle download endpoint
+	http.HandleFunc("/d", downloadPDF) 
 
-	// Initialize the SQLite database
-	db = initDB() // Initialize db here
-	defer db.Close() // Ensure the database is closed when done
+	db = initDB() 
+	defer db.Close() 
 
 	fmt.Println("Server is running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
